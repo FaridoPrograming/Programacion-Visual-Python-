a49aafe0912b038e07ee4df09290c9fff8f5bbc8
Jiménez Soriano Edgar Farid <ekarbodoque@gmail.com>
2026-08-25 13:57:08 -0600

    Update 1.5 Graficas.py
    Se le agrego encabezados a los ejes X y Y

--- 1.5 Graficas.py
+++ 1.5 Graficas.py
@@ -5,4 +5,6 @@ y = [2,4,6,8,10]
 #create plot line
 mpl.plot(x,y,color="green",marker="o",linestyle="-")
 mpl.title("Voltaje vs Corriente")
+mpl.xlabel("Voltaje de la fuente")
+mpl.ylabel("Corriente directa del diodo")
 mpl.show()
